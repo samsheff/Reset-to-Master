cd2a8469a813746f40d429f8a53683dc67298be3
Sam Sheffres <samuel.sheffres@kes.global>
2021-06-28 06:40:21 -0500

    Initial Commit

---
+++ resetToMaster.js
@@ -0,0 +1,20 @@
+i// ==UserScript==
+// @name         Reset to Master
+// // @namespace    https://github.com/
+// // @version      0.1
+// // @description  Change main to master on new github repo pages for easier copy/paste. Because some people are actually required to use 'master' branches for compailibility and compliance reasons.
+// // @author       samsheff
+// // @match        https://github.com/*/
+// // @icon         https://github.com/favicon.ico
+// // @grant        none
+// // ==/UserScript==
+//
+// (function() {
+//     'use strict';
+//
+//         document.querySelectorAll("span.user-select-contain").forEach(function (e) {
+//                 e.innerText = e.innerText.replace(" main", " master");
+//                     });
+//                     })();
+//         })
+// })
